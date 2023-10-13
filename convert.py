ea28e5c7ed59d74b0d46a2fa5eab2ad28edef687
Moray Macdonald <moray.macdonald1@gmail.com>
2023-10-13 14:14:09 +0100

    Read from a directory of input files

--- convert.py
+++ convert.py
@@ -1,12 +1,14 @@
 import sys
+import os
+import glob
 import csv
 import json
 import argparse
 
-parser = argparse.ArgumentParser(description='Process Spotify JSON exports for loading into Universal Scrobbler')
-parser.add_argument('-n', '--num', help='Number of rows to include in each output file', default=2800, type=int)
-parser.add_argument('-o', '--output', help='Output file name stem (will be output as <output_stem>-n.csv', default='output')
-parser.add_argument('input', help='Input file to process')
+parser = argparse.ArgumentParser(description="Process Spotify JSON exports for loading into Universal Scrobbler")
+parser.add_argument("-n", "--num", help="Number of rows to include in each output file", default=2400, type=int)
+parser.add_argument("-o", "--output", help="Output file name stem (will be output as <output_stem>-n.csv", default="output")
+parser.add_argument("input", help="Path to directory with input files to process")
 
 DELIM = ","
 
@@ -19,21 +21,28 @@ def main():
 	input_file_name = args.input
 	output_file_name = args.output
 
-	input_file = open(input_file_name, "r", encoding="UTF8")
-	all_data = json.load(input_file)
-	input_file.close()
+	if not os.path.isdir(args.input):
+		print("Provided path is not a directory")
+		exit()
+
+	input_files = glob.glob(os.path.join(os.path.abspath(args.input), "*.json"))
+
+	all_input_data = []
+	for file in input_files:
+		f = open(file, "r", encoding="UTF8")
+		all_input_data += json.load(f)
+		f.close()
 
 	all_rows = []
 	rows_set = set() # for de-duplication
 
-	for play in all_data:
+	for play in all_input_data:
 		duration_ms = play["ms_played"]
 		artist = play["master_metadata_album_artist_name"]
 		title = play["master_metadata_track_name"]
 		if not artist or not title or duration_ms < 30000:
 			continue
 		row = [artist, title, play["master_metadata_album_album_name"], play["ts"], "", str(int(duration_ms / 1000))]
-		print(row)
 		stringified_row = DELIM.join(row)
 		if stringified_row not in rows_set:
 			all_rows.append(row)
