65ee48a43638c02b49ff93beab271ccd03d3b167
Moray Macdonald <moray.macdonald1@gmail.com>
2023-10-16 20:57:01 +0100

    Filter out plays by date

--- convert.py
+++ convert.py
@@ -4,10 +4,13 @@ import glob
 import csv
 import json
 import argparse
+from datetime import datetime
 
 parser = argparse.ArgumentParser(description="Process Spotify JSON exports for loading into Universal Scrobbler")
 parser.add_argument("-n", "--num", help="Number of rows to include in each output file", default=2400, type=int)
 parser.add_argument("-o", "--output", help="Directory to place output files", default=".")
+parser.add_argument("-f", "--from", help="Only take plays recorded after this date (ISO format)", default=None, dest="from_date")
+parser.add_argument("-t", "--to", help="Only take plays recorded before this date (ISO format)", default=None, dest="to_date")
 parser.add_argument("input", help="Path to directory with input files to process")
 
 DELIM = ","
@@ -17,9 +20,20 @@ def chunk(l, n):
 	for i in range(0, len(l), n):
 		yield l[i:i + n]
 
+def is_naive(d):
+	return d.tzinfo is None or d.tzinfo.utcoffset(d) is None
+
+def ensure_tz(d):
+	if is_naive(d):
+		return d.astimezone()
+	else:
+		return d
+
 def main():
 	args = parser.parse_args()
 	output_path = os.path.abspath(args.output)
+	date_from = ensure_tz(datetime.fromisoformat(args.from_date)) if args.from_date else None
+	date_to = ensure_tz(datetime.fromisoformat(args.to_date)) if args.to_date else None
 
 	if not os.path.isdir(args.input):
 		print("Provided path is not a directory")
@@ -39,11 +53,18 @@ def main():
 	rows_set = set() # for de-duplication
 
 	for play in all_input_data:
+		date_played = ensure_tz(datetime.fromisoformat(play["ts"].replace('Z', '+00:00')))
 		duration_ms = play["ms_played"]
 		artist = play["master_metadata_album_artist_name"]
 		title = play["master_metadata_track_name"]
+
+		# Last.fm needs both an artist and title, and will only count plays over 30s
 		if not artist or not title or duration_ms < 30000:
 			continue
+		# Filter out plays by date
+		if (date_from and date_played >= date_from) or (date_to and date_played <= date_to):
+			continue
+
 		row = [artist, title, play["master_metadata_album_album_name"], play["ts"], "", str(int(duration_ms / 1000))]
 		stringified_row = DELIM.join(row)
 		if stringified_row not in rows_set:
